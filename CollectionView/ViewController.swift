c7d9aa3a777188104e0c9e910a27ef7bff08f2de
fujified <uktras@gmail.com>
2020-12-28 22:15:59 +0700

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by mr spock on 12/28/20.
+//  Copyright © 2020 mr spock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
